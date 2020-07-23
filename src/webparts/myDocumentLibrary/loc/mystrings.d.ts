bd0a6f8adb2d3fe07880445d503fe43b0de5a37b
OceanTech <gayasagarv@gmail.com>
2020-07-23 12:20:53 +0530

    initial Commit

---
+++ src/webparts/myDocumentLibrary/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMyDocumentLibraryWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MyDocumentLibraryWebPartStrings' {
+  const strings: IMyDocumentLibraryWebPartStrings;
+  export = strings;
+}
